6195e711b056074c8bf3226e02f86eb1fdf851de
Jonathan Matondo <johnniumatos@gmail.com>
2026-09-19 15:54:47 +0200

    fix: route websocket function on vercel

--- server/api/ws.js
+++ server/api/ws.js
@@ -5,8 +5,6 @@ const { createWebSocketServer } = require("../src/websocket/websocket.server");
 const app = express();
 const server = http.createServer(app);
 
-// Vercel expose cette Function sur /api/ws.
-// Le serveur WebSocket est attaché directement à cette Function.
 createWebSocketServer(server);
 
 module.exports = server;
